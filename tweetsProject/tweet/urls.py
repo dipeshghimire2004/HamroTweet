a9d1bb15188b54d58e2d9d3a45928992fb491881
dipeshghimire2004 <ghimiredipesh2004@gmail.com>
2024-10-22 19:00:24 +0545

    styling

--- tweetsProject/tweet/urls.py
+++ tweetsProject/tweet/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'), 
     path('', views.tweet_List, name='tweet_List' ),
-    # path('search_results/', views.search_results, name="search_results"),
     path('create/', views.tweet_create, name='tweet_create'),
     path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
     path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
